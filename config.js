a3451d9e3159c772a0a4fe7090b6ff90fc411ddb
keshrivishnu <vishnuksre7970@gmail.com>
2025-05-31 19:04:20 +0530

    Update categories and add new files

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
  export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId:  process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:  process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASURMENT_ID
+    apikey: "AIzaSyBWWnCwcdy02WaZpbNSsgj0fznVgp40aOM",
+authdomain: "jainandsons-3b31c.firebaseapp.com",
+projectId: "jainandsons-3b31c",
+storagebucket: "jainandsons-3b31c.firebasestorage.app",
+messagingsenderid: "760950586458",
+appid: "1:760950586458:web:f881c10ce848fa5c372c63",
+mesurmentid: "G-QZQFDHX03C"
   };
   
   
