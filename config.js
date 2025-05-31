1f19c717eef00723570f98e81b45c9c1cc7ae19f
keshrivishnu <vishnuksre7970@gmail.com>
2025-05-31 19:21:08 +0530

    Update categories and add new files

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
  export const firebaseConfig = {
     apikey: "AIzaSyBWWnCwcdy02WaZpbNSsgj0fznVgp40aOM",
-authdomain: "jainandsons-3b31c.firebaseapp.com",
+authDomain: "jainandsons-3b31c.firebaseapp.com",
 projectId: "jainandsons-3b31c",
-storagebucket: "jainandsons-3b31c.firebasestorage.app",
-messagingsenderid: "760950586458",
-appid: "1:760950586458:web:f881c10ce848fa5c372c63",
-mesurmentid: "G-QZQFDHX03C"
+storageBucket: "jainandsons-3b31c.firebasestorage.app",
+messagingSenderId: "760950586458",
+appId: "1:760950586458:web:f881c10ce848fa5c372c63",
+mesurmentId: "G-QZQFDHX03C"
   };
   
   
